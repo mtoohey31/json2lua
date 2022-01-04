ed704482c3859da9e3bf8e561afd88d2e3ace0a6
Matthew Toohey <contact@mtoohey.com>
2022-01-04 18:18:07 -0500

    ci: remove deploy workflow and combine lint

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,17 +1,11 @@
 import { publish } from "gh-pages";
 
-publish(
-  "dist",
-  {
-    branch: "gh-pages",
-    dotfiles: true,
-    repo: "https://github.com/mtoohey31/json2lua",
-    user: {
-      name: "Matthew Toohey",
-      email: "contact@mtoohey.com",
-    },
+publish("dist", {
+  branch: "gh-pages",
+  dotfiles: true,
+  repo: "git@github.com:mtoohey31/json2lua",
+  user: {
+    name: "Matthew Toohey",
+    email: "contact@mtoohey.com",
   },
-  () => {
-    console.log("Deploy Complete!");
-  }
-);
+});
