608ff8d0c708ce6cb7e58b3ed40eef3cd0353deb
Matthew Toohey <contact@mtoohey.com>
2022-01-04 20:44:14 -0500

    chore: update tailwindcss to v3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,11 @@
-const colors = require("tailwindcss/colors");
-
 const config = {
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {
       fontFamily: {
         sans: ['"IBM Plex Sans"', "sans-serif"],
         mono: ['"JetBrains Mono"', "monospace"],
-      },
-      colors: {
-        orange: colors.orange,
       },
     },
   },
